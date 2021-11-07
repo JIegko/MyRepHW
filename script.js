446d7fe012fbcbfd802c996672ae745cd9c506cc
SerhiiD <so4ek11@gmail.com>
2021-11-07 16:33:20 +0200

    Home work 3

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict'
+let n = +prompt ('Введите номер n-ного члена последовательности Фибоначчи');
+let f0 = 2;
+let f1 = 5;
+function fibonResult (f0, f1, n) {
+   if (n == 0) {
+       return f0;
+   } else if (n == 1) {
+       return f1; 
+   } else if (n > 1) {
+       return fibonResult(f0, f1, n - 1) + fibonResult(f0, f1, n - 2);
+   } else if (n < 0) {
+       return fibonResult(f0, f1, n + 2) - fibonResult(f0, f1, n + 1);
+   }
+}
+alert(`${n}-й член последовательности Фибоначчи, с начальными числами ${f0} и ${f1} равен: ${fibonResult(f0, f1, n)}`);
